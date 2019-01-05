d33a9e2e2ccce2a1cd7d1631bfa58531d3d477c7
yaolin1 <15851853085@163.com>
2019-01-05 11:01:43 +0800

    dialog works

---
+++ vue/3种组件调用方式/dialog/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Dialog title="登陆弹窗" :visible.sync="visible">
+      <form class="form">
+        <div class="form__item">
+          <label>用户名: </label><input type="text" ref="username"/>
+        </div>
+        <div class="form__item">
+          <label>密码: </label><input type="password"/>
+        </div>
+      </form>
+      <div slot="footer" class="footer">
+        <button type="button" @click="visible = false">取消</button>
+        <button type="button" @click="visible = false">确定</button>
+      </div>
+    </Dialog>
+    <button type="button" @click="open">打开弹窗</button>
+  </div>
+</template>
+
+<script>
+import Dialog from './components/dialog.vue'
+
+export default {
+  name: 'app',
+  data(){
+    return {
+      visible: true
+    }
+  },
+  methods: {
+    open(){
+      this.visible = true;
+      this.$nextTick(() => {
+        this.$refs.username.focus();
+      })
+    }
+  },
+  components: {
+    Dialog
+  }
+}
+</script>
+
+<style scoped>
+.form__item{display: flex;margin-top: 5px;}
+.form__item:first-child{margin-top: 0;}
+.form__item label{margin-right: 5px;  width: 4em;text-align: right;}
+.form__item input{flex: 1;}
+.footer{display: flex;height: 100%; justify-content: center;align-items: center;}
+.footer button{margin-left: 10px;}
+</style>
---
+++ vue/3种组件调用方式/dialog/src/components/dialog.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="dialog" v-show="visible">
+    <div class="dialog__modal" @click="close"></div>
+    <div class="dialog__wrap">
+      <div class="dialog__header">
+        <slot name="header">{{title}}</slot>
+      </div>
+      <div class="dialog__body">
+        <slot></slot>
+      </div>
+      <div class="dialog__footer">
+        <slot name="footer"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'mi-dialog',
+  props: {
+    visible: {
+      type: Boolean,
+      default: true
+    },
+    title: {
+      type: String,
+      default: '弹窗'
+    }
+  },
+  methods:{
+    close(){
+      this.$emit('update:visible', false);
+    }
+  }
+}
+</script>
+
+<style scoped>
+.dialog{display: flex; justify-content: center;align-items: center; position: fixed;left: 0; right: 0;top: 0;bottom: 0;}
+.dialog__modal{position: absolute;width: 100%;height: 100%;}
+.dialog__wrap{position: relative; box-sizing: border-box; width: 50vw;background: #ffebd7;border-radius: 4px;}
+.dialog__header{text-align: center; height: 45px;line-height: 45px;color: #fff; background: #353235;}
+.dialog__footer{margin:0 10px; height: 40px;line-height: 40px;border-top: 1px solid #000;}
+.dialog__body{padding: 10px;}
+</style>
