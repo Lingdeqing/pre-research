3059cc776817fbe61952e070108e1e98932eb0c5
姚林 <yaolin1@xiaomi.com>
2020-04-20 17:55:10 +0800

    文件绘制命令

---
+++ 鲁班/文字绘制命令测试.js
@@ -0,0 +1,20 @@
+// 1. 有宽度 有高度
+// 居中
+convert -size 200x100 xc:none -fill 'red' -pointsize 50 -font 'Fonts/PingFangSC-Semibold.ttf' -gravity center -draw 'text 0,0 "哈哈"'  a.png
+
+// 文字要向上微调文字大小的五分之一，才能贴着顶部，避免行高的影响
+// 上左
+convert -size 200x100 xc:none -fill 'red' -pointsize 50 -font 'Fonts/PingFangSC-Semibold.ttf' -gravity northWest -draw 'text 0,-10 "哈哈"'  a.png
+// 上中 
+convert -size 200x100 xc:none -fill 'red' -pointsize 50 -font 'Fonts/PingFangSC-Semibold.ttf' -gravity north -draw 'text 0,-10 "哈哈"'  a.png
+// 上右
+convert -size 200x100 xc:none -fill 'red' -pointsize 50 -font 'Fonts/PingFangSC-Semibold.ttf' -gravity northEast -draw 'text 0,-10 "哈哈"'  a.png
+// 下左
+convert -size 200x100 xc:none -fill 'red' -pointsize 50 -font 'Fonts/PingFangSC-Semibold.ttf' -gravity southWest -draw 'text 0,-10 "哈哈"'  a.png
+// 下右
+convert -size 200x100 xc:none -fill 'red' -pointsize 50 -font 'Fonts/PingFangSC-Semibold.ttf' -gravity southEast -draw 'text 0,-10 "哈哈"'  a.png
+// 下中
+convert -size 200x100 xc:none -fill 'red' -pointsize 50 -font 'Fonts/PingFangSC-Semibold.ttf' -gravity south -draw 'text 0,-10 "哈哈"'  a.png
+
+// 没宽度就靠左侧
+// 没有高度就靠上面
