0292eac452d2e5d267c086e68707b07073f35895
姚林 <yaolin1@xiaomi.com>
2020-04-21 11:45:08 +0800

    修改名字

--- 鲁班/文字绘制命令测试.js
+++ 鲁班/文字绘制命令测试.js
@@ -23,4 +23,4 @@ convert -size 200x100 xc:none -fill 'red' -pointsize 50 -font 'Fonts/PingFangSC-
 
 convert -size 200x100 xc:none -fill 'red' -pointsize 50 -font 'Fonts/PingFangSC-Semibold.ttf' -gravity north -draw 'text 0,-10 "哈哈"'  Semibold.png
 convert -size 500x500 xc:none -fill 'red' -pointsize 50 -font 'Fonts/PingFangSC-Light.ttf' -gravity north -draw 'text 0,-10 "立即查看"' -font 'Fonts/PingFangSC-Regular.ttf' -draw 'text 0,50 "立即查看"' -font 'Fonts/PingFangSC-Medium.ttf' -draw 'text 0,100 "立即查看"' -font 'Fonts/PingFangSC-Semibold.ttf' -draw 'text 0,150 "立即查看"' -font 'Fonts/PingFangSC-Bold.ttf' -draw 'text 0,200 "立即查看"'  RegularThin.png
-convert -size 500x500 xc:none -fill 'red' -pointsize 60 -font 'Fonts/fzlt2.ttf' -gravity center -draw 'text 0,-10 "运动户外"' -font 'Fonts/fzlt.ttf' -pointsize 40  -draw 'text 0,50 "立即查看"'  RegularThin.png
+convert -size 500x500 xc:none -fill 'red' -pointsize 60 -font 'Fonts/fzlt-bold.ttf' -gravity center -draw 'text 0,-10 "运动户外"' -font 'Fonts/fzlt-thin.ttf' -pointsize 40  -draw 'text 0,50 "立即查看"'  RegularThin.png
