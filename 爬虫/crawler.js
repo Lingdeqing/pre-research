d40f0edc0829f972c31e53255cbda19170fa6b99
yaolin <15851853085@163.com>
2024-07-08 21:59:18 +0800

    1

--- 爬虫/crawler.js
+++ 爬虫/crawler.js
@@ -24,7 +24,7 @@ async function fetchPage({ url, prevFileName, onNextFetched }) {
         let nextPageLink = $('#pb_next').attr('href');
         let nextFileName = ''
         let currentIndex = pageIndex
-        let currentFileName = `${currentIndex}_${title.replace(/\s+/, '_')}.md`
+        let currentFileName = `${currentIndex.toString().padStart(4, 0)}_${title.replace(/\s+/, '_')}.md`
         onNextFetched?.({
             title,
             fileName: currentFileName
@@ -36,6 +36,7 @@ async function fetchPage({ url, prevFileName, onNextFetched }) {
             if (outputed) return
             outputed = true
             const footerHtml = `${nextFileName ? `<p><a href="./${encodeURIComponent(nextFileName)}">下一页</a></p>` : ''}${prevFileName ? `<p><a href="./${encodeURIComponent(prevFileName)}">上一页</a></p>` : ''}<p><a href="../">目录</a></p>`
+            console.log(`河神/${currentFileName}`)
             fse.outputFile(path.resolve(__dirname, `河神/${currentFileName}`), `<h1>${title}</h1>
             <div>${footerHtml}</div>
             <div>${articleHtml}</div>
